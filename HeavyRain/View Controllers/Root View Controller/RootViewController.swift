1d3e16291f593db7f7070f55111e775ced6f04f5
UOA1992 <UOA1992@github.com>
2018-08-06 17:07:40 -0400

    Initial Commit

---
+++ HeavyRain/View Controllers/Root View Controller/RootViewController.swift
@@ -0,0 +1,20 @@
+//
+//  RootViewController.swift
+//  HeavyRain
+//
+//  Created by Edward L Richardson on 8/6/18.
+//  Copyright © 2018 Edward L Richardson. All rights reserved.
+//
+
+import UIKit
+
+final class RootViewController: UIViewController {
+    
+    //Mark: - View Life Cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        }
+    }
+
